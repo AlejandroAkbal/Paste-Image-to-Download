a138652ca31a82eed137313185b837676a01ee5d
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-04-13 17:47:56 +0200

    feat: use title and description from locale

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+const { t } = useI18n()
+
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: t('app.title'),
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: t('app.description') },
   ],
 })
 </script>
