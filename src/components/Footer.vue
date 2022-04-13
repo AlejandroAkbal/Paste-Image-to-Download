bf6e7f295fee986b3db13a85f11870b24af4fd7b
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-04-13 18:28:31 +0200

    feat: reorganize footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,35 +1,36 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import { toggleDark } from '~/composables'
 
 const { t, availableLocales, locale } = useI18n()
 
 const toggleLocales = () => {
-  // change to some real logic
   const locales = availableLocales
   locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
 }
 </script>
 
 <template>
-  <nav text-xl mt-6>
-    <RouterLink class="icon-btn mx-2" to="/" :title="t('button.home')">
-      <div i-carbon-campsite />
+  <nav class="mt-6 text-center text-xl">
+    <RouterLink :title="t('button.home')" class="icon-btn mx-2" to="/">
+      <div class="i-carbon-campsite" />
     </RouterLink>
 
-    <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
-      <div i="carbon-sun dark:carbon-moon" />
-    </button>
-
-    <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales">
-      <div i-carbon-language />
-    </a>
-
-    <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
-      <div i-carbon-dicom-overlay />
+    <RouterLink :title="t('button.about')" class="icon-btn mx-2" to="/about">
+      <div class="i-carbon-dicom-overlay" />
     </RouterLink>
 
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
-      <div i-carbon-logo-github />
-    </a>
+    <div class="mt-2">
+      <button :title="t('button.toggle_dark')" class="icon-btn mx-2 !outline-none" @click="toggleDark()">
+        <div class="i-carbon-sun dark:i-carbon-moon" />
+      </button>
+
+      <a class="icon-btn mx-2" href="https://github.com/AlejandroAkbal/Paste-Image-to-Download" rel="noreferrer" target="_blank" title="GitHub">
+        <div class="i-carbon-logo-github" />
+      </a>
+
+      <a :title="t('button.toggle_langs')" class="icon-btn mx-2" @click="toggleDark()">
+        <div class="i-carbon-language" />
+      </a>
+    </div>
   </nav>
 </template>
